cefbd9797ebbdd58693ac2ce095873d0db732c66
shubhaks79 <ksshubharao@gmail.com>
2017-01-24 10:31:11 +0530

    Create UIApplicationConstants.java

---
+++ src/com/sales/ui/util/UIApplicationConstants.java
@@ -0,0 +1,9 @@
+package com.sales.ui.util;
+
+import java.awt.Font;
+
+public class UIApplicationConstants {
+	public static Font applicationFont=new Font("Arial",0,12);
+	 public static Font tableHdrFont=new Font("Arial",0,12);
+	 public static Font panelTitleFont=new Font("Arial",0,12);
+}
