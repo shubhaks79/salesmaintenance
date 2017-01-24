0e0947a0844a955141017e6cbc8a82d0373d7212
shubhaks79 <ksshubharao@gmail.com>
2017-01-24 10:28:49 +0530

    Create BaseEntity.java

---
+++ src/com/sales/entity/BaseEntity.java
@@ -0,0 +1,12 @@
+package com.sales.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.Embeddable;
+
+@Embeddable
+public class BaseEntity implements Serializable,Cloneable{
+	private static final long serialVersionUID = 1L;
+
+
+}
