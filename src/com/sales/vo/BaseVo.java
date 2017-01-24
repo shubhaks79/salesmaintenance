856eedc505a9365334de2a586d31cc9703dbf45c
shubhaks79 <ksshubharao@gmail.com>
2017-01-24 10:32:56 +0530

    Create BaseVo.java

---
+++ src/com/sales/vo/BaseVo.java
@@ -0,0 +1,54 @@
+package com.sales.vo;
+
+import java.sql.Timestamp;
+
+public abstract class BaseVo {
+	private int id;
+	private String createUser;
+	
+	private Timestamp createDate;
+	
+	private String modifiedUser;
+	
+	private Timestamp modifiedDate;
+
+	public String getCreateUser() {
+		return createUser;
+	}
+
+	public void setCreateUser(String createUser) {
+		this.createUser = createUser;
+	}
+
+	public Timestamp getCreateDate() {
+		return createDate;
+	}
+
+	public void setCreateDate(Timestamp createDate) {
+		this.createDate = createDate;
+	}
+
+	public String getModifiedUser() {
+		return modifiedUser;
+	}
+
+	public void setModifiedUser(String modifiedUser) {
+		this.modifiedUser = modifiedUser;
+	}
+
+	public Timestamp getModifiedDate() {
+		return modifiedDate;
+	}
+
+	public void setModifiedDate(Timestamp modifiedDate) {
+		this.modifiedDate = modifiedDate;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+}
